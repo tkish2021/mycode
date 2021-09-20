643d8a6c2d14ae9599490fefb44af28505650512
Timothy Kish <tkish2021@users.noreply.github.com>
2021-09-20 21:24:16 +0000

    print(reason)

--- shove.py
+++ shove.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import os
+#get the reason for update from user. This is a string catch of what an updated is being pushed
 print("What is the reason for your update")
 reason = input()
 
+pre = 'git status'
 cmd = 'cd ~/mycode;git add *;git commit -m "print(reason)";git push origin;'
-os.system(cmd)
-#print("`(home_dir)/mycode;git add *;git commit -m \"(reason)\",git push origin;`") 
+post = 'git status'
 
+#os commands for system. Take variables from commands pre cmd and post
+os.system(pre)
+os.system(cmd)
+os.system(post)
