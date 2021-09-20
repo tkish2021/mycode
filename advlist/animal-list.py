158b77246f93b92d8936d17e959a1e5a0eadb36d
Timothy Kish <tkish2021@users.noreply.github.com>
2021-09-20 17:47:28 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+list1 = ["Fox","Fly","Ant","Bee","Cod","Cat","Dog","Yak","Cow","Hen","Koi","Hog","Jay","Kit"]
+print(list1)
+print(list1[4])
+print(list1[10])
+print("list of fish")
+listfish = [list1[4],list1[10]]
+print(listfish)
