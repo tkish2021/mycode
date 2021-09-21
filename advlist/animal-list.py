5952472b68c3d7d27e4711b148041a3235194304
Timothy Kish <tkish2021@users.noreply.github.com>
2021-09-21 15:15:25 +0000

    testing

--- advlist/animal-list.py
+++ advlist/animal-list.py
@@ -7,3 +7,15 @@ print(list1[10])
 print("list of fish")
 listfish = [list1[4],list1[10]]
 print(listfish)
+
+print("list of insects")
+listbugs = [list1[1],list1[2],list1[3]]
+print(listbugs)
+
+print("list of mammals")
+listmammals = [list1[0],list1[5],list1[6],list1[7],list1[8],list1[11],list1[13]]
+print(listmammals)
+
+print("list of birds")
+listbirds = [list1[9],list1[12]]
+print(listbirds)
