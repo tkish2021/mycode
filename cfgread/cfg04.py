b7e3188d56f44b1bf1bc9345c9fec170d245b1cd
Timothy Kish <tkish2021@users.noreply.github.com>
2021-09-22 20:39:37 +0000

    reading in data

---
+++ cfgread/cfg04.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+## create file object in "r"ead mode
+myfile = input("What config file:")
+print(f'You entered {myfile}')
+with open(f'{myfile}', "r") as configfile:
+    ## readlines() creates a list by reading target
+    ## file line by line
+    configlist = configfile.readlines()
+    count = len(open("vlanconfig.cfg", "r").readlines()) 
+    
+## file was just auto closed (no more indenting)
+
+## each item of the list now has the "\n" characters back
+print(configlist)
+print("Number of Lines:", count)
+
