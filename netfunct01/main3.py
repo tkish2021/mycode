7814f065a884ce7b11a6bec700c94f1b9d1a2ac6
Timothy Kish <tkish2021@users.noreply.github.com>
2021-09-23 14:29:28 +0000

    Learning about functions

--- netfunct01/main3.py
+++ netfunct01/main3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Alta3 Research || Author: RZFeeser@alta3.com"""
 
+import json
+
 # function to push commands
 def commandpush(devicecmd): # devicecmd==dict
 
@@ -15,10 +17,8 @@ def commandpush(devicecmd): # devicecmd==dict
 # start our main script
 def main():
     """called at runtime"""
-
-    # dict containing IPs mapped to a list of physical interfaces and their state
-    devicecmd = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1":
-    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]}
+    with open("devicecmd.json", "r") as devicecmdfile:
+        devicecmd = json.load(devicecmdfile) # decode the JSON from the file to pythonic data
 
     print("Welcome to the network device command pusher") # welcome message
 
