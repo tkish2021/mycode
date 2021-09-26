5c416bdde5bc16cd8c0178bb1c72a6752ca9bdf9
Timothy Kish <tkish2021@users.noreply.github.com>
2021-09-26 20:43:41 +0000

    cert

--- alta3research-python-cert/matplot.py
+++ alta3research-python-cert/matplot.py
@@ -27,21 +27,25 @@ def main():
     PINNAPLES = int(input() )
 
     fruits = ('Bananas', 'Apples', 'Oranges', 'Pinnaples')
-    y_pos = np.arange(len(fruits))
     numfruits = [ BANANAS , APPLES , ORANGES , PINNAPLES ]
+    
+    y_pos = np.arange(len(fruits)) #Sets auto number scaling based on user number inputs for axes Y
+    #x_pos = np.arange(len(fruits))
 
-    plt.bar(y_pos, numfruits, align='center', alpha=0.5)
-    plt.xticks(y_pos, fruits)
-    plt.ylabel('Number of Fruit')
-    plt.title('How many Fruit in the basket')
+    plt.bar(y_pos, numfruits, align='center', alpha=0.5) #Sets Y position for Each Fruit
+    #plt.bar(x_pos, fruits, color=['yellow', 'red', 'orange', 'brown'])
+    plt.xticks(y_pos, fruits) #Ticks are based off from Auto Scaling
+    plt.ylabel('Number of Fruit') #Basic Label for Y Axes
+    plt.title('How many Fruit in the basket') #Title for the Graph
 
-    plt.show()
-    # plt.show() # you can try this on a Python IDE with a GUI if you'd like
-    plt.savefig("/home/student/alta3research-python-cert/Fruit.png")
+    plt.show() #Displays the Graph
+    plt.savefig("/home/student/alta3research-python-cert/Fruit.png")  #This just saves the graph file
     print("The graph is located /home/student/alta3research-python-cert/Fruit.png")
-    # save a copy to "~/static" (the "files" view)
-    plt.savefig("/home/student/static/Fruit.png")
+    #This just prints out the location for the user to get the graph
+
+    plt.savefig("/home/student/static/Fruit.png") #This saves an additional copy so we can view it on lab server in static
     print("Also saved a copy here /home/student/static/Fruit.png")
+    #This print just informs the user where the file is 
 
     
 if __name__ == "__main__":
